3ffcd297bb3cc476891d95cf7f19cb9aaa5b344e
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-08-23 19:33:44 +0700

    distance

--- app/Http/Controllers/admin/SignInStatusController.php
+++ app/Http/Controllers/admin/SignInStatusController.php
@@ -86,8 +86,13 @@ class SignInStatusController extends Controller
                 } else {
                     $rowspan = '';
                 }
+
                 $full_name = $row->employee->fname . ' ' . $row->employee->mname . ' ' . $row->employee->lname;
                 $address = $row->sing_in?((activity_($row, 'lat') ?? $row->project->lat) . "," . (activity_($row, 'lon') ?? $row->project->lon)):'null';
+
+                $distance = $this->haversineDistance(activity_($row, 'lat'), activity_($row, 'lon'), $row->project->lat, $row->project->lon);
+                $distance = $row->sing_in? round($distance, 2) : '0'; //Kilometer
+                
                 $html .= "
                         <tr class='".($row->sing_in && Carbon::parse($row->shift_start)>=Carbon::parse($row->sing_in)?'':'bg-light-danger')."'>
                         $rowspan
@@ -108,12 +113,36 @@ class SignInStatusController extends Controller
                             <td>
                                 <img src='" . 'https://api.eazytask.au/' . img_(activity_($row, 'sign_out')) . "' alt='' height='60px' width='70px'>
                             </td>
+                            <td>" . $row->signin_comment . "</td>
+                            <td>" . $row->signout_comment . "</td>
                             <td>" . $row->project->pName . "</td>
-                            <td><a href='https://www.google.com/maps/search/$address' target='blank'>$address</a></td>
+                            <td><a href='https://www.google.com/maps/search/$address' target='blank'>$address</a> <br>Distance: ($distance) KM</td>
                           </tr>
             ";
             }
         }
         return response()->json(['data' => $html]);
+    }
+
+    private function haversineDistance($lat1, $lon1, $lat2, $lon2) {
+        $earthRadius = 6371; // Radius of the Earth in kilometers
+    
+        $lat1Rad = deg2rad($lat1);
+        $lon1Rad = deg2rad($lon1);
+        $lat2Rad = deg2rad($lat2);
+        $lon2Rad = deg2rad($lon2);
+    
+        $latDiff = $lat2Rad - $lat1Rad;
+        $lonDiff = $lon2Rad - $lon1Rad;
+    
+        $a = sin($latDiff / 2) * sin($latDiff / 2) +
+             cos($lat1Rad) * cos($lat2Rad) *
+             sin($lonDiff / 2) * sin($lonDiff / 2);
+    
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+    
+        $distance = $earthRadius * $c; // Distance in kilometers
+    
+        return $distance;
     }
 }
