dac787d1fa98ff7999f04e1c86e239bd2d2cb448
Marvin Dave Gajultos <marvindavegajultosdev@gmail.com>
2024-01-23 20:23:10 +0800

    Velzon Initial Setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,199 +1,101 @@
-<!DOCTYPE html>
-<html class="loading" lang="en" data-textdirection="ltr">
-<!-- BEGIN: Head-->
+@extends('layouts.VelzonAdmin.master-without-nav')
+@section('title')
+@lang('translation.signin')
+@endsection
+@section('content')
+<div class="auth-page-wrapper pt-5">
+    <!-- auth page bg -->
+    <div class="auth-one-bg-position auth-one-bg"  id="auth-particles">
+        <div class="bg-overlay"></div>
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Easytask/roster, a personalized and simple way to get more done. All projects and lists customized to you.">
-    <meta name="keywords" content="Easytask.au Scheduler roster">
-    <meta name="author" content="Ahsan">
-    <title>Online Scheduler</title>
-    <link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('app-assets/images/ico/favicon.ico')}}">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+        <div class="shape">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+                <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+            </svg>
+        </div>
+    </div>
 
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/vendors.min.css')}}">
-    <!-- END: Vendor CSS-->
+    <!-- auth page content -->
+    <div class="auth-page-content">
+        <div class="container">
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card mt-4">
 
-    <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/bootstrap-extended.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/colors.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/components.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/themes/dark-layout.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/themes/bordered-layout.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/themes/semi-dark-layout.css')}}">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/menu/menu-types/vertical-menu.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/plugins/forms/form-validation.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/pages/page-auth.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/toastr.min.css')}}">
-    <!-- END: Page CSS-->
-
-    <!-- <script type="text/javascript" src="{{ asset('backend') }}/lib/toastr/toastr.min.js"></script> -->
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"> -->
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
-    <!-- END: Custom CSS-->
-
-</head>
-<!-- END: Head-->
-
-<!-- BEGIN: Body-->
-
-<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="blank-page">
-    <!-- BEGIN: Content-->
-    @include('sweetalert::alert')
-    <div class="app-content content ">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
-        <div class="content-wrapper">
-            <div class="content-header row">
-            </div>
-            <div class="content-body">
-                <div class="auth-wrapper auth-v2">
-                    <div class="auth-inner row m-0">
-                        <!-- Brand logo--><a class="brand-logo" href="javascript:void(0);">
-                            
-                       <!-- <img src="{{asset('images/app/logo.png')}}" alt="" height="100px" class="ml-2"> -->
-                            <!--<h2 class="brand-text text-primary ml-1">Roster</h2>-->
-                        </a>
-                        <!-- /Brand logo-->
-                        <!-- Left Text-->
-                        <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
-                            <div class="w-100 d-lg-flex align-items-center justify-content-center px-5"><img class="img-fluid" src="{{asset('app-assets/images/pages/login-v2.svg')}}" alt="Login V2" /></div>
-                        </div>
-                        <!-- /Left Text-->
-                        <!-- Login-->
-                        <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
-                            <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-                                <h2 class="card-title font-weight-bold mb-1">Welcome to Online Scheduler!</h2>
-                                <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
-                                <form class="auth-login-form mt-2" method="POST" action="{{ route('login') }}">
+                        <div class="card-body p-4">
+                            <div class="text-center mt-2">
+                                <h5 class="text-primary">Welcome to Online Scheduler!</h5>
+                                <p class="text-muted">Please sign-in to your account and start the adventure.</p>
+                            </div>
+                            <div class="p-2 mt-4">
+                                <form action="{{ route('login') }}" method="POST">
                                     @csrf
-                                    <div class="form-group">
-                                        <label class="form-label" for="login-email">Email</label>
-
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <div class="mb-3">
+                                        <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control @error('email') is-invalid @enderror" id="username" name="email" placeholder="Enter username">
                                         @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="d-flex justify-content-between">
-                                            <label for="login-password">Password</label>
-
-                                            @if (Route::has('password.request'))
-                                            <a href="{{ route('password.request') }}"><small>Forgot Password?</small></a>
-                                            @endif
-                                        </div>
-                                        <div class="input-group input-group-merge form-password-toggle @error('password') is-invalid @enderror">
-                                            <input id="password" type="password" class="form-control @error('password') error @enderror" name="password" required autocomplete="current-password">
-                                            <div class="input-group-append"><span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span></div>
-                                        </div>
-                                            
-                                        @error('password')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
-                                            @enderror
-                                        <!-- <span id="password-error" class="error">This field is required.</span> -->
+                                        @enderror
                                     </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox mb-3">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                            <label class="custom-control-label" for="customCheck">Remember Me</label>
+
+                                    <div class="mb-3">
+                                        <div class="float-end">
+                                            <a href="{{ route('password.update') }}" class="text-muted">Forgot password?</a>
                                         </div>
-
+                                        <label class="form-label" for="password-input">Password <span class="text-danger">*</span></label>
+                                        <div class="position-relative auth-pass-inputgroup mb-3">
+                                            <input type="password" class="form-control password-input pe-5 @error('password') is-invalid @enderror" name="password" placeholder="Enter password" id="password-input">
+                                            <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
                                     </div>
-                                    <button class="btn btn-gradient-primary btn-block" tabindex="4">Sign in</button>
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="auth-remember-check" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                                    </div>
+
+                                    <div class="mt-4">
+                                        <button class="btn btn-success w-100" type="submit">Sign In</button>
+                                    </div>
                                 </form>
-
-
-                                <!-- <div class="mt-1">
-                                    <a href="#"><img src="{{asset('images/app/ios.png')}}" style="width: 250px;" alt=""></a>
-                                    <a href="{{asset('apk/android.apk')}}" download=""><img src="{{asset('images/app/android.png')}}" style="width: 250px;" alt=""></a>
-                                </div> -->
-                                <div class="mt-1">
-                                    <a href="/privacy/policy" class="mr-2">Privacy Policy</a>
-                                    <a href="/terms/and/conditions">Terms & Conditions</a>
-                                </div>
                             </div>
                         </div>
-                        <!-- /Login-->
+                        <!-- end card body -->
+                    </div>
+                    <!-- end card -->
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end auth page content -->
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- END: Content-->
+    </footer>
+    <!-- end Footer -->
+</div>
+@endsection
+@section('script')
+<script src="{{ URL::asset('build/libs/particles.js/particles.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/particles.app.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/password-addon.init.js') }}"></script>
 
-
-    <!-- BEGIN: Vendor JS-->
-    <script src="{{asset('app-assets/vendors/js/vendors.min.js')}}"></script>
-    <!-- BEGIN Vendor JS-->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- BEGIN: Page Vendor JS-->
-    <script src="{{asset('app-assets/vendors/js/forms/validation/jquery.validate.min.js')}}"></script>
-    <!-- END: Page Vendor JS-->
-
-    <!-- BEGIN: Theme JS-->
-    <script src="{{asset('app-assets/js/core/app-menu.js')}}"></script>
-    <script src="{{asset('app-assets/js/core/app.js')}}"></script>
-    <!-- END: Theme JS-->
-
-    <!-- BEGIN: Page JS-->
-    <script src="{{asset('app-assets/js/scripts/pages/page-auth-login.js')}}"></script>
-    <!-- END: Page JS-->
-
-    <script>
-        $(window).on('load', function() {
-            if (feather) {
-                feather.replace({
-                    width: 14,
-                    height: 14
-                });
-            }
-        })
-    </script>
-
-    <!-- <script type="text/javascript" src="{{ asset('backend') }}/lib/toastr/toastr.min.js"></script>
- -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script> -->
-
-    <script>
-        @if(Session::has('message'))
-
-        var type = "{{Session::get('alert-type','info')}}"
-        switch (type) {
-            case 'info':
-                toastr.info(" {{Session::get('message')}} ");
-                break;
-            case 'success':
-                toastr.success(" {{Session::get('message')}} ");
-                break;
-            case 'warning':
-                toastr.warning(" {{Session::get('message')}} ");
-                break;
-            case 'error':
-                toastr.error(" {{Session::get('message')}} ");
-                break;
-        }
-        @endif
-    </script>
-
-</body>
-<!-- END: Body-->
-
-</html>
+@endsection
